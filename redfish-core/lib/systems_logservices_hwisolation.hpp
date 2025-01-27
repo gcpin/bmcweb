472bf95e55bde52076fa7d5036450c1ed91459d1
deepakala-k <112156780+deepakala-k@users.noreply.github.com>
2025-01-27 19:56:55 +0530

    HW-Isolation: Fix Additional Data Uri (#1128)
    The guard records could be associated with hidden or visible PEL. The
    additional URI for visible pel would be
    /redfish/v1/Systems/system/LogServices/EventLog/Entries/<id>/attachment
    and for the hidden ones it has be
    /redfish/v1/Systems/system/LogServices/CELog/Entries/<id>/attachment
    
    So before adding the AdditionalDataURI, check for the hidden property of
    the PEL and add the URI accordingly.
    
    Tested and found it works as expected
    
    ```
    {
      "@odata.id": "/redfish/v1/Systems/system/LogServices/HardwareIsolation/Entries",
      "@odata.type": "#LogEntryCollection.LogEntryCollection",
      "Description": "Collection of System Hardware Isolation Entries",
      "Members": [
        {
          "@odata.id": "/redfish/v1/Systems/system/LogServices/HardwareIsolation/Entries/1",
          "@odata.type": "#LogEntry.v1_9_0.LogEntry",
          "AdditionalDataURI": "/redfish/v1/Systems/system/LogServices/CELog/Entries/824/attachment",
          "Created": "2025-01-23T12:57:02+00:00",
          "EntryType": "Event",
          "Id": "1",
          "Links": {
            "OriginOfCondition": {
              "@odata.id": "/redfish/v1/Systems/system/Processors/dcm0-cpu0/SubProcessors/core0"
            }
          },
          "Message": "Spare Guard Record core0",
          "MessageArgs": [
            "Spare",
            "core0"
          ],
          "MessageId": "OpenBMC.0.6.GuardRecord",
          "Name": "Hardware Isolation Entry",
          "Severity": "OK"
        },
        {
          "@odata.id": "/redfish/v1/Systems/system/LogServices/HardwareIsolation/Entries/3",
          "@odata.type": "#LogEntry.v1_9_0.LogEntry",
          "AdditionalDataURI": "/redfish/v1/Systems/system/LogServices/EventLog/Entries/830/attachment",
          "Created": "2025-01-23T13:08:31+00:00",
          "EntryType": "Event",
          "Id": "3",
          "Links": {
            "OriginOfCondition": {
              "@odata.id": "/redfish/v1/Systems/system/Processors/dcm0-cpu1/SubProcessors/core0"
            }
          },
          "Message": "Spare Guard Record core0",
          "MessageArgs": [
            "Spare",
            "core0"
          ],
          "MessageId": "OpenBMC.0.6.GuardRecord",
          "Name": "Hardware Isolation Entry",
          "Severity": "OK"
        },
    }
    ```

--- redfish-core/lib/systems_logservices_hwisolation.hpp
+++ redfish-core/lib/systems_logservices_hwisolation.hpp
@@ -631,7 +631,6 @@ inline void fillSystemHardwareIsolationLogEntry(
              : asyncResp->res.jsonValue);
 
 	    std::string guardType;
-	        bool hiddenPEL = false;
      // We need the severity details before getting the associations
      // to fill the message details.
 
@@ -665,7 +664,6 @@ inline void fillSystemHardwareIsolationLogEntry(
                              "xyz.openbmc_project.HardwareIsolation.Entry.Type.Spare")
                     {
                          entryJson["Severity"] = "OK";
-			 hiddenPEL = true;
                      }
                 }
             }
@@ -729,14 +727,31 @@ inline void fillSystemHardwareIsolationLogEntry(
                             sdbusplus::message::object_path errPath =
                                 std::get<2>(assoc);
 
-                             std::string logPath = "EventLog";
-                             if (hiddenPEL)
-                             {
-                                 logPath = "CELog";
-                             }
-                            entryJson["AdditionalDataURI"] = boost::urls::format(
-				                                "/redfish/v1/Systems/system/LogServices/{}/Entries/{}/attachment",
-                                 logPath, errPath.filename());
+				                            std::string entryID = errPath.filename();
+
+                             auto updateAdditionalDataURI = [asyncResp,
+                                                             entryJsonIdx,
+                                                             entryID](
+                                                                bool hidden) {
+                                 nlohmann::json& entryJsonToupdateURI =
+                                     (entryJsonIdx > 0
+                                          ? asyncResp->res
+                                                .jsonValue["Members"]
+                                                          [entryJsonIdx - 1]
+                                          : asyncResp->res.jsonValue);
+                                 std::string logPath = "EventLog";
+
+                                 if (hidden)
+                                 {
+                                     logPath = "CELog";
+                                 }
+                                 entryJsonToupdateURI["AdditionalDataURI"] =
+                                     boost::urls::format(
+                                         "/redfish/v1/Systems/system/LogServices/{}/Entries/{}/attachment",
+                                         logPath, entryID);
+                             };
+                             getHiddenPropertyValue(asyncResp, entryID,
+                                                    updateAdditionalDataURI);
                         }
                     }
                 }
